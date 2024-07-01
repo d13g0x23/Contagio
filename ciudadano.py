62f89b38aa8c2d9313ef463258a44d661e0ddf1b
Diego <146681634+d13g0x23@users.noreply.github.com>
2024-07-01 17:27:32 -0400

    se modifico el atributo estado en la clase ciudadano  indicando True si esta sano o muerto

--- ciudadano.py
+++ ciudadano.py
@@ -1,6 +1,6 @@
 class Ciudadano:
-    def __init__(self, id, nombre, apellido, comunidad, familia, estado='S'):
-        self.id = id 
+    def __init__(self, id, nombre, apellido, comunidad, familia, estado=True):
+        self.id = id
         self.nombre = nombre
         self.apellido = apellido
         self.comunidad = comunidad
@@ -10,13 +10,14 @@ class Ciudadano:
         self.infectado = False
         self.recuperado = False
         self.pasos_infectado = 0
-    
-    def infectarse(self):
+
+    def infectar(self):
         if self.susceptible:
             self.susceptible = False
             self.infectado = True
+            self.recuperado = False
             self.pasos_infectado = 0
-
+    
     def recuperar(self):
         self.infectado = False
         self.recuperado = True
