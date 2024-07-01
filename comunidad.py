7902b45c5bd120c75cc84004d377f686f7a1a96c
Diego <146681634+d13g0x23@users.noreply.github.com>
2024-07-01 17:23:44 -0400

    Se agrego un metodo para crear ciudadanos en la clase comunidad a partir del json

--- comunidad.py
+++ comunidad.py
@@ -1,3 +1,10 @@
+import json
+import random
+from ciudadano import Ciudadano
+
+with open('nombres&apellidos.json', 'r', encoding='utf-8') as file:
+    datos = json.load(file)
+
 class Comunidad:
     def __init__(self, num_ciudadanos, promedio_conexion_fisica, enfermedad, num_infectados, probabilidad_conexion_fisica):
         self.num_ciudadanos = num_ciudadanos
@@ -6,4 +13,15 @@ class Comunidad:
         self.num_infectados = num_infectados
         self.probabilidad_conexion_fisica = probabilidad_conexion_fisica
         self.ciudadanos = []
+    
+    def crear_ciudadanos_desde_json(self):
+        nombres = datos['nombres']
+        apellidos = datos['apellidos']
+
+        for i in range(self.num_ciudadanos):
+            nombre = random.choice(nombres)
+            apellido = random.choice(apellidos)
+            id = i + 1
+            ciudadano = Ciudadano(id, nombre, apellido, self, None)
+            self.ciudadanos.append(ciudadano)
         
